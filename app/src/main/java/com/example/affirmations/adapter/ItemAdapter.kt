a2917b6fcacfbb7f4d942c74469e9bf598cc4f42
Kanaishk Garg <kanaishkgarg@gmail.com>
2022-09-19 20:04:42 +0530

    edited Affirmation, DataSource amd Item Adapter to include ImageView and created MainActivity. Also implemented MaterialCardView to display list_item.

--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -1,26 +1,33 @@
 package com.example.affirmations.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
 import com.example.affirmations.model.Affirmation
+import com.google.android.material.card.MaterialCardView
 
 class ItemAdapter(
     private val context: Context,
-    private val dataset: List<Affirmation>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    private val dataset: MutableList<Affirmation>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        TODO("Not yet implemented")
+        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(adapterLayout)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount() = dataset.size
--- app/src/main/java/com/example/affirmations/data/DataSource.kt
+++ app/src/main/java/com/example/affirmations/data/DataSource.kt
@@ -6,16 +6,16 @@ import com.example.affirmations.model.Affirmation
 class DataSource {
     fun loadAffirmations() : MutableList<Affirmation> {
         return mutableListOf<Affirmation>(
-            Affirmation(R.string.affirmation_1),
-            Affirmation(R.string.affirmation_2),
-            Affirmation(R.string.affirmation_3),
-            Affirmation(R.string.affirmation_4),
-            Affirmation(R.string.affirmation_5),
-            Affirmation(R.string.affirmation_6),
-            Affirmation(R.string.affirmation_7),
-            Affirmation(R.string.affirmation_8),
-            Affirmation(R.string.affirmation_9),
-            Affirmation(R.string.affirmation_10)
+            Affirmation(R.string.affirmation_1,R.drawable.image1),
+            Affirmation(R.string.affirmation_2,R.drawable.image2),
+            Affirmation(R.string.affirmation_3,R.drawable.image3),
+            Affirmation(R.string.affirmation_4,R.drawable.image4),
+            Affirmation(R.string.affirmation_5,R.drawable.image5),
+            Affirmation(R.string.affirmation_6,R.drawable.image6),
+            Affirmation(R.string.affirmation_7,R.drawable.image7),
+            Affirmation(R.string.affirmation_8,R.drawable.image8),
+            Affirmation(R.string.affirmation_9,R.drawable.image9),
+            Affirmation(R.string.affirmation_10,R.drawable.image10)
         )
     }
 }
