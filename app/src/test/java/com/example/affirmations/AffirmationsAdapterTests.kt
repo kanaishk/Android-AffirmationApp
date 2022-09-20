fef797f7cdc7f493598002d69167a1e7daefb0e1
Kanaishk Garg <kanaishkgarg@gmail.com>
2022-09-20 11:31:55 +0530

    Added instrumentation test and unit test. Removed the default ones. Added dependencies to build.gradle

---
+++ app/src/test/java/com/example/affirmations/AffirmationsAdapterTests.kt
@@ -0,0 +1,23 @@
+package com.example.affirmations
+
+import android.content.Context
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.model.Affirmation
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class AffirmationsAdapterTests {
+
+    private val context = mock(Context::class.java)
+
+    @Test
+    fun adapter_size() {
+        val data = mutableListOf(
+            Affirmation(R.string.affirmation_1, R.drawable.image1),
+            Affirmation(R.string.affirmation_2, R.drawable.image2)
+        )
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
+}
